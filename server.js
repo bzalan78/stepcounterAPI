3c45ac743a60e63d8a43e89305afa16b3d83f753
bzalan78 <bokanyizalan@turr.hu>
2026-09-14 11:18:21 +0200

    first version

---
+++ server.js
@@ -0,0 +1,107 @@
+const express =require('express')
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  port: 3307,
+  database: 'stepcounter'
+
+})
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the step counter API')
+})
+
+//users endpoint
+
+
+//registration
+app.post('/users/register', (req, res) => {
+    const {name, password, email, confirm} = req.body
+    console.log(name, password, email, confirm)
+    //validate input
+    if(!name || !password || !email || !confirm) {
+      return res.status(400).json({ error: 'All fields are required' })
+    }
+    if(password !== confirm) {
+      return res.status(400).json({ error: 'Passwords do not match' })
+    }
+    //check passwd strength
+
+    //check if email already exists
+    pool.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+        if (results.length > 0) {
+            return res.status(400).json({ error: 'Email already exists' })
+        }
+        //register user
+        pool.query('INSERT INTO users (name, password, email) VALUES (?, ?, ?)', [name, password, email], (error, results) => {
+            if (error) {
+                return res.status(500).json({ error: 'Internal server error' })
+            }
+            res.status(200).json({ message: 'User registered successfully' })
+        })
+    })
+
+})
+
+//login
+
+//logout ?
+
+//password change
+
+//get profile
+
+//update profile
+
+//delete profile
+
+
+
+
+
+//steps endpoint
+
+
+//create step
+
+//get step
+
+//update step
+
+//delete step
+
+
+//admin endpoint
+
+
+//get all users
+app.get('/admin/users', (_req, res) => {
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      return res.status(500).json({ error: 'Internal server error' })
+    }
+    else{
+      res.status(200).json(results)
+    }
+   
+  })
+})
+
+//deny user
+
+//statistics
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
